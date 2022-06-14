64505131634ffe8344e6dbf4300e36753dcec2fd
Josh Overbeek <joshoverbeek0@gmail.com>
2022-06-14 14:02:36 -0400

    Documentation changes and Cargo.toml update

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Overbeek Audio 2
 
 ### Powered by Tauri and Svelte
+
+To use, simply click the share link under a YouTube video and copy the part of the URL highlighted here:
+![Youtube Share URL](/public/youtubeurl.png "YouTube Share URL")
+
+Then paste that into the "id" box on the app, add a nickname for the audio, and click "Add Video"
--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,48 @@
 <script lang="ts">
   export const name: string = "Overbeek Audio";
-  import { each } from "svelte/internal";
 
+  // import our Audio element and file io requirements
   import AudioBox from "./AudioBox.svelte";
   import { save, open } from "@tauri-apps/api/dialog";
   import { readTextFile, writeFile } from "@tauri-apps/api/fs";
 
   let currentID: string = "";
   let currentNickname: string = "";
-  let audios = []; //d4eRXTWTkAU
-  let targetedFile = "";
+  let audios = [];
 
+  /**
+   * Add a new Audio element based on the current values of the id and nickname field
+   * then clear those fields.
+   */
   function onAdd() {
+    // this syntax forces the app to reload what audio boxes are visable
     audios = [...audios, { id: currentID, nickname: currentNickname }];
     currentID = "";
     currentNickname = "";
   }
 
+  /**
+   * Predefined callback function for deleting AudioBoxes
+   * @param id The video ID
+   * @param nickname The nickname of the video
+   */
   function del(id: String, nickname: String) {
+    // filters the list to only contain the elements that don't match
     audios = audios.filter((a) => a.id !== id && a.nickname != nickname);
   }
 
+  /**
+   * Clears all audioboxes
+   */
   function clear() {
     audios = [];
     currentID = "";
     currentNickname = "";
   }
 
+  /**
+   * Handles saving the file
+   */
   function saveDialog() {
     let pathToFile = save({
       title: "Select a file to save to",
@@ -34,11 +50,13 @@
     });
 
     pathToFile.then((value: string) => {
-      console.log(JSON.stringify(audios));
       writeFile({ path: value, contents: JSON.stringify(audios) });
     });
   }
 
+  /**
+   * Handles loading a file
+   */
   function loadDialog() {
     let pathToFile = open({
       title: "Select a file to load",
@@ -63,6 +81,7 @@
     <button on:click={clear}>Clear Videos</button>
   </div>
   <div class="audio-zone">
+    <!-- Loads each audio in the audio list as an audio box. This is reloaded on assignment -->
     {#each audios as audio}
       <AudioBox nickname={audio.nickname} id={audio.id} onDelete={del} />
     {:else}
